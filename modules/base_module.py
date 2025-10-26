924f3722ffc944dbf65aa89271cac0990c9f8ea4
Bot <dikov39@gmail.com>
2025-10-26 22:05:24 +0000

    Update base_module.py

--- modules/base_module.py
+++ modules/base_module.py
@@ -1,31 +1,23 @@
 from PyQt6.QtWidgets import QWidget
-from abc import ABC, abstractmethod
+from abc import ABC
 from core.event_bus import EventBus
 
-class BaseModule(ABC):
-    """Базовый класс для всех модулей"""
-    
-    def __init__(self, event_bus: EventBus, dependencies: dict = None):
-        self.event_bus = event_bus
-        self.dependencies = dependencies or {}
-        self._setup_event_handlers()
-    
-    @abstractmethod
-    def _setup_event_handlers(self):
-        """Настройка обработчиков событий"""
-        pass
-
-class BaseTabModule(BaseModule, QWidget):
+class BaseTabModule(QWidget, ABC):
     """Базовый класс для модулей-вкладок"""
     
     TAB_NAME = "Unnamed Tab"
     
     def __init__(self, event_bus: EventBus, dependencies: dict = None):
         QWidget.__init__(self)
-        BaseModule.__init__(self, event_bus, dependencies)
+        self.event_bus = event_bus
+        self.dependencies = dependencies or {}
+        self._setup_event_handlers()
         self._create_ui()
     
-    @abstractmethod
+    def _setup_event_handlers(self):
+        """Настройка обработчиков событий (можно переопределить)"""
+        pass
+    
     def _create_ui(self):
-        """Создает UI компонент"""
+        """Создает UI компонент (должен быть переопределен)"""
         pass
